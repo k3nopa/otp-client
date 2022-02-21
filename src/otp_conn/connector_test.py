36873bfe105a052b36104b31f10ee0a8289ebdda
k3nopa <afiqnaufalpro@gmail.com>
2022-02-21 14:57:33 +0000

    otp key generation updated

--- src/otp_conn/connector_test.py
+++ src/otp_conn/connector_test.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python3 
 
 import otp_conn as oc
+import secrets
 
 layer = 128
-height = 7
+height = 6
+k = 80
 
-print(f"Receiving Decision tree, Layer: {layer}, Height: {height}")
-data = oc.recv_tree(layer, height)
-print(f"Decision Tree: {data}")
+# print(f"Receiving Decision tree, Layer: {layer}, Height: {height}")
+# data = oc.recv_tree(layer, height)
+# print(f"Decision Tree: {data}")
+
+print(f"Generate OTP key from L:{layer}, H:{height}")
+path = secrets.randbits(height)
+fail_count = 0
+while True:
+  otp = oc.generate_otp_key(path, k)
+  print(f"OTP: {otp}")
+  if otp == "":
+    print(f"Failed!, {fail_count}")
+    fail_count += 1
+  if fail_count == 11:
+    break
--- src/otp_conn/src/lib.rs
+++ src/otp_conn/src/lib.rs
@@ -87,9 +87,100 @@ fn recv_tree(mut layer: usize, height: usize) -> io::Result<Vec<Vec<u32>>> {
     Ok(trees)
 }
 
-/// A Python module implemented in Rust.
+#[pyfunction]
+fn generate_otp_key(path: u8, k: u32) -> io::Result<String> {
+    let port_name = "/dev/ttyUSB0";
+    let baud_rate = 115200;
+
+    let builder = serialport::new(port_name, baud_rate)
+        .stop_bits(serialport::StopBits::One)
+        .data_bits(serialport::DataBits::Eight)
+        .timeout(Duration::from_millis(10));
+
+    let port = builder.open();
+    let mut otp_key = String::new();
+
+    match port {
+        Ok(mut port) => {
+            // 1. Send path.
+            port.write(&[path]).unwrap();
+
+            let mut receiving = true;
+            let mut layer = 0;
+            // 2. Receive layer's length.
+            let mut buf = [0; 4];
+            while receiving {
+                match port.read(&mut buf) {
+                    Ok(_) => {
+                        let mut buffer = io::Cursor::new(buf);
+                        layer = buffer.read_u32::<BigEndian>().unwrap();
+                        println!("Layer: {}", layer);
+                        receiving = false;
+                    }
+                    Err(ref e) if e.kind() == io::ErrorKind::TimedOut => {}
+                    Err(e) => {
+                        eprintln!("{:?}", e);
+                        receiving = false;
+                    }
+                }
+            }
+
+            // 3. Receive OTP key.
+            let mut dbg = true;
+            let mut failed = false;
+            if layer < k {
+                failed = true;
+            }
+            while layer > 0 && dbg {
+                let mut status = Status::RECV;
+                if status == Status::RECV {
+                    let mut buf = [0; 4]; // Key size is 4 bytes.
+                    let mut recv_key = String::new();
+                    receiving = true;
+
+                    while receiving {
+                        match port.read(&mut buf) {
+                            Ok(_) => {
+                                if !failed {
+                                    let mut buffer = io::Cursor::new(buf);
+                                    let recv_data = buffer.read_u32::<BigEndian>().unwrap();
+                                    recv_key = format!("{}", recv_data);
+                                    otp_key.push_str(&recv_key);
+                                } else {
+                                    otp_key.push_str("");
+                                }
+
+                                status = Status::SEND;
+                                receiving = false;
+                            }
+                            Err(ref e) if e.kind() == io::ErrorKind::TimedOut => {}
+                            Err(e) => {
+                                eprintln!("{:?}", e);
+                                receiving = false;
+                                dbg = false;
+                            }
+                        }
+                    }
+                }
+
+                if status == Status::SEND {
+                    port.write(&[1]).unwrap();
+                    status = Status::RECV;
+                }
+                layer -= 1;
+            }
+        }
+        Err(e) => {
+            eprintln!("Failed to open \"{}\". Error: {}", port_name, e);
+            ::std::process::exit(1);
+        }
+    }
+    Ok(otp_key)
+}
+
 #[pymodule]
 fn otp_conn(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(generate_otp_key, m)?)?;
     m.add_function(wrap_pyfunction!(recv_tree, m)?)?;
     Ok(())
 }
